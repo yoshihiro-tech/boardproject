336c9bcef5e37318c8021350e39c39b0902342a2
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-07-05 13:47:21 +0900

    Apply Bootstrap and create the prototype of listView

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from boardapp.views import signupfunc
+from boardapp.views import loginfunc
+from boardapp.views import listfunc
 
 
 urlpatterns = [
-    path('signup/', signupfunc),
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('list/', listfunc, name='list'),
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 
 
+def signupfunc(request):
+    if request.method == 'POST':
+        new_username = request.POST['username']
+        new_password = request.POST['password']
+        try:
+            User.objects.get(username=new_username)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(new_username, '', new_password)
+            return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html', {'some': 100})
 
-def signupfunc(requet):
-    return render(requet, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        new_username = request.POST['username']
+        new_password = request.POST['password']
+        user = authenticate(request, username=new_username,
+                            password=new_password)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def listfunc(request):
+    return render(request, 'list.html')
